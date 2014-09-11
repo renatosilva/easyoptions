4aca0ae67a3291671cca5894e49f8218e884c54f
Renato Silva <br.renatosilva@gmail.com>
2014-09-11 16:45:28 -0300

    New Bash front end to the Ruby implementation.
    The pure Bash implementation is now installed as easyoptions.sh instead of just easyoptions, which is now a front end to the Ruby implementation.

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 from=$(dirname "$0")
 
 if [[ "$1" != --remove ]]; then
-    cp -v "$from/easyoptions.sh" /usr/local/bin/easyoptions
+    cp -v "$from/easyoptions"    /usr/local/bin
+    cp -v "$from/easyoptions.sh" /usr/local/bin
     cp -v "$from/easyoptions.rb" /usr/local/bin
 else
     rm -vf /usr/local/bin/easyoptions
+    rm -vf /usr/local/bin/easyoptions.sh
     rm -vf /usr/local/bin/easyoptions.rb
 fi
