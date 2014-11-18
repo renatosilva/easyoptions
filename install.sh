f50b37f6fb2157923699c3eac2421daa965e44fb
Renato Silva <br.renatosilva@gmail.com>
2014-11-17 23:18:53 -0200

    Small fix for the installation script.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ from=$(dirname "$0")
 where="${1:-/usr/local/bin}"
 mkdir -p "$where" || exit
 
-if [[ "$1" != --remove ]]; then
+if [[ "$2" != --remove ]]; then
     cp -v "$from/easyoptions"     "$where"
     cp -v "$from/easyoptions.sh"  "$where"
     cp -v "$from/easyoptions.rb"  "$where"
